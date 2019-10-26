1e63ab426e4e3246d3c7a5111fc8e1ecb50a8abd
Jacob DeCamp <jmdnht@mail.umsl.edu>
2019-10-26 17:50:48 -0500

    all js in seperate files

--- listValues.js
+++ listValues.js
@@ -16,3 +16,37 @@ function generateSearch() {
    }
    //console.log(divOutputs);
 }
+
+
+
+
+
+
+
+
+
+
+
+function onLoad() {
+   getCSVdata();
+   generateSearch();
+}
+
+function generateSearch() {
+   var divOutputs = "";
+   var i = 0;
+
+   //console.log(cols);
+   //console.log(cols.lenght)
+
+   for (i = 0; i < cols.length; i++) {
+      //  console.log(i);
+      let options = document.createElement("option");
+      options.value = cols[i];
+      options.innerHTML = cols[i];
+      document.getElementById("properties").appendChild(options);
+      // options += "<options value=cols[j]>cols[j]</options>" ;
+      // console.log(cols[i]);
+   }
+   //console.log(divOutputs);
+}
