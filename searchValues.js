899c7c44a274d652a5f1784c1c8d635031819a8a
Jacob DeCamp <jmdnht@mail.umsl.edu>
2019-10-26 23:26:10 -0500

    getting up to date

--- searchValues.js
+++ searchValues.js
@@ -1,34 +1,29 @@
-function searchValues()
-{
+function searchValues() {
     var property = document.getElementById("properties").value;
     var propValue = document.getElementById("elements").value;
 
     var i = 0;
     var matchedListing = Array();
-    for (i = 0; i < listings.length; i++)
-    {
-        if(listings[i][property] == propValue)
-        {
+    for (i = 0; i < listings.length; i++) {
+        if (listings[i][property] == propValue) {
             matchedListing.push(listings[i]);
         }
     }
 
     var narratives = Array();
 
-    for(i=0; i < matchedListing.length; i++)
-    {
+    for (i = 0; i < matchedListing.length; i++) {
         //Comes from ai code
-        //narratives.push(general_narrative(matchedListing[i]));
+        narratives.push(general_narrative(matchedListing[i]));
 
-        narratives.push(i);
+        // narratives.push(i);
 
         //for
 
         document.getElementById("foundListings").innerHTML += matchedListing[i] + "\n";
     }
 
-    sessionStorage.setItem("narratives", JSON.stringify(narratives));
-
-    window.location.href = "./page2.html";
+    document.write(narratives);
+    console.log(narratives);
 
 }
